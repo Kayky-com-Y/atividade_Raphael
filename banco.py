5cbfb9cd85471e1f6103268ee5510fb993597ab2
Kayky-com-Y <KaykyDaSilvaBatista2020@gmail.com>
2024-07-23 09:01:09 -0300

    Banco_kayky

---
+++ banco.py
@@ -0,0 +1,33 @@
+class conta ():
+    _lista_clientes = ["cliente_sem_nome"]
+    nome_cliente = str("cliente_sem_nome")
+    __dinheiro = float(0)
+    def dizer_nome(self):
+        print(f"""
+    ===========================================
+        O nome do cliente é {self.nome_cliente}
+    ===========================================
+    """)
+    def mudar_nome(self):
+        self.nome_cliente = str(input("Escreva o novo nome do cliente"))
+        return self.nome_cliente
+    
+    def dizer_lista_de_clientes(self):
+        for cliente in self._lista_clientes:
+            print(f"Cliente: {cliente}")
+            
+    def adicionar_lista_de_clientes(self):
+        return self._lista_clientes.append(self.nome_cliente)
+        
+    def dizer_dinheiro(self):
+        return print(f"Vc tem {self.__dinheiro} no banco")
+
+    def Deposite_dinheiro(self):
+        depositar = float(input("Deposite R$:"))
+        self.__dinheiro += depositar
+        return self.__dinheiro
+    
+    def retire_dinheiro(self):
+        retirar = float(input("Retire R$:"))
+        self.__dinheiro -= retirar
+        return self.__dinheiro
